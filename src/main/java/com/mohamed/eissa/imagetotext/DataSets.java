96a207baabfd0538b100be044c7079a861210c54
mohamedeissa3000 <107439278+mohamedeissa3000@users.noreply.github.com>
2022-06-14 01:46:12 +0200

    first commit

---
+++ src/main/java/com/mohamed/eissa/imagetotext/DataSets.java
@@ -0,0 +1,6 @@
+package com.mohamed.eissa.imagetotext;
+
+public class DataSets {
+    public static final short n1=1000;
+    public static final double n2=(1000*Math.max(n1,10000))*Math.PI;
+}
---
+++ src/main/java/com/mohamed/eissa/imagetotext/issaEquals.java
@@ -0,0 +1,8 @@
+package com.mohamed.eissa.imagetotext;
+import android.os.Bundle;
+
+import java.beans.IndexedPropertyChangeEvent;
+public interface issaEquals {
+     Bundle m_mohamedIssaSum(Class<Object> nameClass,String keyBundle);
+    DataSets m_mohamedIssa_DataSets(DataSets myDataClass,Bundle data);
+}
